68676112de542179e012844f4743be38f74fe3be
Sam <sharrisdev@gmail.com>
2025-06-06 13:08:12 +0100

    updated settings.py for Heroku deployment

--- project/settings.py
+++ project/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool('DEBUG', default=False)
 
 ALLOWED_HOSTS = []
 
